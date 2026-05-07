41b997a788dd9b1a7cd28d5520db33a8d8f3d3a5
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 19:25:57 +0530

    commit

--- app.py
+++ app.py
@@ -4,25 +4,38 @@ import requests
 st.set_page_config(layout="wide")
 st.title("POC Concept for Vigil")
 
+output = ""
+
 action_box,output_box=st.columns(2,border=True)
 
 with action_box:
     task_select=st.selectbox("Select Task","pre_health_checks",width="stretch")
     instance_select=st.selectbox("Select Instance",("cust_instance1","cust_instance2"),width="stretch")
+    
     run_clicked = st.button("Run", type="primary")
+
     if run_clicked:
         url = "http://54.198.10.240:8000/tasks/pre-health-check/execute"
-        payload = {"instance_name": instance_select,"task_name": task_select}
-        # Sending the POST request
+
+        payload = {
+            "instance_name": instance_select,
+            "task_name": task_select
+        }
+
         response = requests.post(url, json=payload)
-        
+
         if response.status_code == 200:
+            output = response.json()["output"]
             st.success("Data sent successfully!")
+
         else:
-            st.error(f"Failed to send data: {response.status_code}")
+            output = f"Failed to send data: {response.status_code}"
+            st.error(output)
 
 with output_box:
-        st.title("output")
-        container = st.container(height=600,border=False)
-        with container:
-            st.code("output", language="bash",height="stretch")
+    st.title("output")
+
+    container = st.container(height=600,border=False)
+
+    with container:
+        st.code(output, language="bash")
