4d4c0fd63a289112f1877a013186170e19d0ac15
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 16:54:21 +0530

    commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("POC Concept for Vigil")
 action_box,output_box=st.columns(2,border=True)
 
 with action_box:
-    
+    task_select=st.selectbox(label="Select Task",width="stretch")
 
 with output_box:
     st.title("output")
