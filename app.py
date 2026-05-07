983f04b74bc0b1caa74bf9a1c2ed590c43c2b467
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 16:31:52 +0530

    commit

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ st.set_page_config(layout="wide")
 st.title("POC Concept for Vigil")
 
 action_box,output_box=st.columns(2,border=True)
+
+with action_box:
+    task_select,instance_select,progress=st.columns(3,border=True,vertical_alignment="bottom")
+
+with output_box:
+    st.title("output")
+    txt=st.text_area("andncnsndjncjsdnclw")
