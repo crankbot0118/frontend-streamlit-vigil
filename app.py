24d708039c6d9e1b00fec5202eaa10b1702ac65c
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 17:01:49 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.title("POC Concept for Vigil")
 
-action_box,output_box=st.columns(2,border=True,height=600)
+action_box,output_box=st.columns(2,border=True)
 
 with action_box:
     task_select=st.selectbox("Select Task","pre_health_checks",width="stretch")
