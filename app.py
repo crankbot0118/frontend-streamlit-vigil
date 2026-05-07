4d7073a479f62de48b7bff489644d7c8784f1229
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 16:57:02 +0530

    commit

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ action_box,output_box=st.columns(2,border=True)
 
 with action_box:
     task_select=st.selectbox("Select Task","pre_health_checks",width="stretch")
+    instance_select=st.selectbox("Select Instance","pre_health_checks",width="stretch")
+    st.button("Run", type="primary")
 
 with output_box:
     st.title("output")
