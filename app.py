24208d6659d4b03d4f1d283fe203b54538fe0b08
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 16:47:46 +0530

    commit

--- app.py
+++ app.py
@@ -10,13 +10,10 @@ col1, col2, col3 = st.columns(3,
 vertical_alignment="bottom")
 
 with col1:
-    st.metric("CPU Usage", "24%", delta="Load Avg:
-0.45")
+    st.metric("CPU Usage", "24%", delta="Load Avg:0.45")
 
 with col2:
-    st.metric("Memory Used", "3.2 GB", delta="-12 GB
-free")
+    st.metric("Memory Used", "3.2 GB", delta="-12 GBfree")
 
 with col3:
-    st.metric("Disk Usage", "68%", delta="120 GB
-free")
+    st.metric("Disk Usage", "68%", delta="120 GBfree")
