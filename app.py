56cd06de42826c73c2370539825c65985f121af8
crankbot0118 <tarunsathwikpalepu0118m2@gmail.com>
2026-05-07 16:52:11 +0530

    commit

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-# import streamlit as st
-
-# st.set_page_config(layout="wide")
-# st.title("POC Concept for Vigil")
-
-# # action_box,output_box=st.columns(2,border=True)
-
-# # with action_box:
-# #     task_select,instance_select,progress=st.columns(3,border=True,vertical_alignment="bottom")
-
-# # with output_box:
-# #     st.title("output")
-# #     st.code("adcbjs cjnqec", language="bash")
-
-
-# left, middle, right = st.columns(3)
-
-# left.text_input("Write something")
-# middle.button("Click me", use_container_width=True)
-# right.checkbox("Check me")
-
 import streamlit as st
 
-col1, col2 = st.columns(2)
+st.set_page_config(layout="wide")
+st.title("POC Concept for Vigil")
 
-with col1:
-    st.write("Top of Column 1")
-    st.button("Bottom of Column 1")
+action_box,output_box=st.columns(2,border=True)
+
+with action_box:
+    
+
+with output_box:
+    st.title("output")
+    st.code("adcbjs cjnqec", language="bash")
